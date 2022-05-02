5b1bb2fd9de20c0557658af8e2de0a695965afa4
Nithin Gopal <nithingopal2015@gmail.com>
2022-05-02 23:11:59 +0530

    Add html CSS skeleton
    - added html skeleton
    - added TW classes
    - script to be added

---
+++ public/scripts/forecast.js
@@ -0,0 +1,1 @@
+console.log('forcast.js test');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+      },
+      fontFamily: {
+        'title' : ['Water Brush'],
+        'Montserrat' : ['Montserrat'],
+        'NanumGothic' : ['Nanum Gothic'],
+        'Nunito' : ['Nunito']
+      }
+    },
   },
   plugins: [],
 }
