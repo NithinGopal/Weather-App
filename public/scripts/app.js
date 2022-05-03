4fa93220e44893cb6473d9d032ce6d96955eba05
Nithin Gopal <nithingopal2015@gmail.com>
2022-05-03 12:24:07 +0530

    Add functionality
    - made app & forcast.js for DOM &
    - added script in forcast.js for API collect.
    - added script in app.js for DOM manup.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,68 @@
-console.log('app.js test');
+// This script file is for DOM manipulation
+
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateUI = (data) => {
+
+    console.log(data)
+    const cityDetails = data.cityDetails;  // make local varaibles with same name
+    const weather = data.weather;
+
+    // update details template
+    details.innerHTML = `<h5 class="my-3 font-bold">${cityDetails.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="text-4xl my-6">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    </div>`;
+
+    // Update day/night & icon images
+    const iconSrc = `./images/icons/${weather.WeatherIcon}.svg`
+    let timeSrc = null;
+    // Day/Night time icons
+    if (weather.IsDayTime) {
+        timeSrc = './images/day.svg';
+    } else {
+        timeSrc = './images/night.svg';
+    }
+    time.setAttribute('src', timeSrc);
+    // weather Icon
+    icon.setAttribute('src', iconSrc);
+
+    // remove hidden class if present
+    if(card.classList.contains('hidden')) {
+        card.classList.remove('hidden');
+    }
+}
+
+const updateCity = async (city) => {
+    // console.log(city)
+    const cityDetails = await getCity(city);  // runs getCity from forecast.js API file
+    const weather = await getWeather(cityDetails.Key); // gets weather data
+
+    //console.log('cityDetails: ', cityDetails);
+    //console.log('weather: ', weather);
+    return {      // returns a promise. not data
+        cityDetails : cityDetails,
+        weather : weather
+    };
+
+    //return {cityDetails, weather};  // Object shorthand
+}
+
+cityForm.addEventListener('submit', e => {
+    e.preventDefault();   // to prevent default action
+
+    // get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();  // resets form after submission
+
+    // update the ui with city name
+    updateCity(city)
+        .then(data => updateUI(data))  // to access data from updateCity promise
+        .catch(err => console.log(err));
+})
--- public/scripts/forecast.js
+++ public/scripts/forecast.js
@@ -1,1 +1,44 @@
-console.log('forcast.js test');
+// This script file is to interact with the weather APIs
+
+// API Key from accuweather
+const key = 'nys6dG4ig5XIPLIDXGAL8nO1jA09hjd4';
+
+// Get weather information from current conditions API with city key 
+const getWeather = async (locationId) => {   // gets locationId from getCity fn.
+    // Resource URL from accuweather for current weather
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${locationId}?apikey=${key}`;
+    // Query parameters // refer under resource url in accuweather
+    const response = await fetch(base + query);
+    const data = await response.json();
+    
+    //console.log(data);  
+    return data[0];   // returns a promise, not data.
+}
+
+// Get the city key with city API
+const getCity = async (city) => {
+
+    // Resource URL from accuweather for city search
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    
+    // Query parameters // refer under resource url in accuweather
+    const query = `?apikey=${key}&q=${city}`;  // takes the API Key and the search value
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    //console.log(data);    // gives all possible searches related to the city name
+    //console.log(data[0]); // gives the popular search option
+    return data[0];         // returns a promise, not data.
+};
+
+getCity('manchester').then(data => {  // to get city key after getCity promise
+    //console.log(data.Key)  
+    return getWeather(data.Key);      // uppercase K in Key
+}).then(data => {                     // to get data from getWeather promise
+    //console.log(data);
+}).catch(err => console.log(err));    // in case of errors
+
+
+//getWeather('329260');
